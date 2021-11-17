f2c37e8480fe68fabc57dab03e4dab8bcf937102
Pablo Hernandez Hernandez <pablohernandezhernandezuacm@gmail.com>
2021-11-17 07:48:57 -0600

    Funcion con botones

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,4 +1,6 @@
 import "../pages/scss/gobal.scss";
+import "semantic-ui-css/semantic.min.css";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,14 @@
+
+import React from "react";
+import {Button,Icon}  from "semantic-ui-react";
+
 export default function Home() {
   return (
     <div className="home">
       <h1>Estamos en Next JS</h1>
-    </div>
-  )
+        <button >Primary</button>
+        <button >Secondary</button>
+  
+      </div>
+  );
 }
