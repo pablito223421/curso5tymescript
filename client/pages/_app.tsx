4f9fc09159930dd4a623ee351b4ea0c4d1999f94
Pablo Hernandez Hernandez <pablohernandezhernandezuacm@gmail.com>
2021-11-17 05:45:30 -0600

    funcion con SCSS como remplazo de css

---
+++ client/pages/_app.tsx
@@ -0,0 +1,6 @@
+import "../pages/scss/gobal.scss";
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
--- client/pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="home">
-      <h1>Estamos en Next JS</h1>
-    </div>
-  )
-}
