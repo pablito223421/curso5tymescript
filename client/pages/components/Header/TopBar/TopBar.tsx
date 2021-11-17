1d61dddac5c0b7a7dcc07110f5ee1f71f5839690
Pablo Hernandez Hernandez <pablohernandezhernandezuacm@gmail.com>
2021-11-17 13:21:18 -0600

    Uso de image con TopBar

--- client/pages/components/Header/TopBar/TopBar.tsx
+++ client/pages/components/Header/TopBar/TopBar.tsx
@@ -1,9 +1,30 @@
 
+import Link from "next/link";
+import Image from "next/image";
 
 export default function TopBar() {
     return (
-        <div>
-          <h1>TopBar ...</h1>  
+        <div className="top-bar">
+         <div>
+        <div className="top-bar">
+        <div  className="top-bar__left">
+        <Logo/>
         </div>
-    )
+        <div  className="top-bar__right">
+        <h2>Buscador</h2>
+        </div>
+        </div>
+         </div> 
+        </div>
+    );
+}
+
+function Logo() {
+return(
+<Link href="/">
+<a>
+<img  src="./logo.png" alt="Gamming"/>
+</a>
+</Link>
+)
 }
