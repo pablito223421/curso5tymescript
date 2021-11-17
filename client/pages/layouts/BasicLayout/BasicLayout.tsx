7d4725575a4a7b2928ea6749df6d740ba9e19494
Pablo Hernandez Hernandez <pablohernandezhernandezuacm@gmail.com>
2021-11-17 10:45:21 -0600

    Uso del topbar y menu con Header

--- client/pages/layouts/BasicLayout/BasicLayout.tsx
+++ client/pages/layouts/BasicLayout/BasicLayout.tsx
@@ -1,9 +1,11 @@
 
+import Header from  "../../components/Header";
 
 export default function BasicLayout(props){
 const {children} = props;
 return(
     <div>
+    <Header />
    <h2 className="content">{children}</h2>
     </div>
 );
