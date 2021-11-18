aaf5579d4bc2e9fca0cfb50dc228fd588be92d59
Pablo Hernandez Hernandez <pablohernandezhernandezuacm@gmail.com>
2021-11-18 16:51:10 -0600

    Register Form con scss para mejorar formulario

--- client/pages/components/Auth/RegisterForm/RegisterForm.tsx
+++ client/pages/components/Auth/RegisterForm/RegisterForm.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import {Form, Button} from "semantic-ui-react";
 
 export default function RegisterForm(props) {
     const {showLoginForm}= props;
     return (
-        <div>
-          <h1>Estamos en el registro de usuarios</h1>
-          <button onClick={showLoginForm}>Ir a Login</button>
-        </div>
+       <Form>
+        <Form.Input name="name" type="text" placeholder="Nombre"/>
+        <Form.Input name="lastname" type="text" placeholder="Apellidos"/>
+        <Form.Input name="username" type="text" placeholder="Nombre de usuario"/>
+        <Form.Input name="email" type="text" placeholder="Correo electrónico"/>
+        <Form.Input name="password" type="password" placeholder="Contraseña"/>
+      <div className="actions">
+          <Button type="button"basic>
+              Iniciar sesión
+          </Button>
+          <Button type="submit" className="submit">
+              Registrar
+          </Button>
+      </div>
+      </Form>
     );
 }
