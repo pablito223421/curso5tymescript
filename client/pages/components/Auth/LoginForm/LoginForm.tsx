4df471918abb18282e0bc6fdd1aa0a6a7bf8e948
Pablo Hernandez Hernandez <pablohernandezhernandezuacm@gmail.com>
2021-11-19 08:49:40 -0600

    Formulario de Login con estilos

--- client/pages/components/Auth/LoginForm/LoginForm.tsx
+++ client/pages/components/Auth/LoginForm/LoginForm.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-
+import React,{useState} from "react";
+import {Form, Button} from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import {toast} from "react-toastify";
 export default function LoginForm(props) {
     const {showRegisterForm}=props;
     return (
-        <div>
-           <h1>Estamos en el registro de usuarios</h1> 
-           <button onClick={showRegisterForm}>Ir al registro</button>
-        </div>
+       <Form>
+           <Form.Input 
+           name="identifier"
+           type="text"
+           placeholder="Correo Electrónico"
+           />
+           <Form.Input 
+           name="password"
+           type="password"
+           placeholder="Contraseña"
+           />
+           <div className="actions">
+              <Button type="button" basic onClick={showRegisterForm}>
+               Registrarse   
+              </Button>
+              <div>
+                  <Button className="submmit" type="submit">
+                      Entrar
+                  </Button>
+                  <Button type="button">¿Has olvidado la contraseña?</Button>
+              </div>
+           </div>
+       </Form>
     )
 }
--- client/pages/components/Auth/RegisterForm/RegisterForm.tsx
+++ client/pages/components/Auth/RegisterForm/RegisterForm.tsx
@@ -57,7 +57,7 @@ export default function RegisterForm(props) {
         onChange={formik.handleChange}/>
         error={formik.errors.password}
       <div className="actions">
-          <Button type="button"basic>
+          <Button type="button" basic onClick={showLoginForm}>
               Iniciar sesión
           </Button>
           <Button type="submit" className="submit" loading ={loading}>
