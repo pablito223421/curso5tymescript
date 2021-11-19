2534aadb75524d08de5cfc662613de45915e4983
Pablo Hernandez Hernandez <pablohernandezhernandezuacm@gmail.com>
2021-11-19 10:18:33 -0600

    Cambio y agregar cambios en el usuario y contraseña

--- client/pages/components/Auth/LoginForm/LoginForm.tsx
+++ client/pages/components/Auth/LoginForm/LoginForm.tsx
@@ -3,19 +3,33 @@ import {Form, Button} from "semantic-ui-react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import {toast} from "react-toastify";
+
 export default function LoginForm(props) {
     const {showRegisterForm}=props;
+
+    const formik= useFormik({
+        initialValues:initialValues(),
+        validationSchema: Yup.object(validationSchema()),
+        onSubmit: (formData) =>{
+          console.log(formData);
+        }
+    });
+
     return (
-       <Form>
+       <Form className="login-form" onSubmit={formik.handleSubmit}>
            <Form.Input 
            name="identifier"
            type="text"
            placeholder="Correo Electrónico"
+           onChange={formik.handleChange}
+           error={formik.errors.identifier}
            />
            <Form.Input 
            name="password"
            type="password"
            placeholder="Contraseña"
+           onChange={formik.handleChange}
+           error={formik.errors.password}
            />
            <div className="actions">
               <Button type="button" basic onClick={showRegisterForm}>
@@ -30,4 +44,18 @@ export default function LoginForm(props) {
            </div>
        </Form>
     )
+}
+
+function initialValues(){
+    return {
+        identifier:"",
+        password:"",
+    };
+}
+
+function validationSchema(){
+ return{
+     identifier :Yup.string().email("true").required("true"),
+     password: Yup.string().required("true"),
+ };
 }
